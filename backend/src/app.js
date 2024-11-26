24fb5de39716868210b8a2a7098862a7c7c938ec
Ayoub-ELHanda <elhandaayo@gmail.com>
2024-11-26 13:23:04 +0100

    updat structor

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,19 +1,21 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 
-// MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.error(err));
+// MongoDB connection
+mongoose
+    .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error(err));
 
-// Example Route
-app.get("/", (req, res) => {
-  res.send("E-Book Platform Backend is running!");
+// Example route
+app.get('/', (req, res) => {
+    res.send('Backend is working!');
 });
 
 const PORT = process.env.PORT || 5000;
---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>Welcome to the E-Book Platform</h1>
+        <router-view></router-view>
+    </div>
+</template>
+<script>
+export default {
+    name: "App",
+};
+</script>
